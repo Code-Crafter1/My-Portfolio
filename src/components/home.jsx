ea5f83533c36f38036bbdeb313b84adb27ced590
Code-Crafter1 <sakshikumarizen@gmail.com>
2026-03-05 20:00:42 +0530

    resume updated done

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,1 +1,60 @@
-c
+import { useNavigate } from "react-router-dom";
+
+import profileImg from "../assets/images/portfolio3.jpg";
+import resumePDF from "../assets/images/sakshi.pdf";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleConnectClick = () => {
+    navigate("/", { state: { scrollTo: "contact" } });
+  };
+
+  return (
+    <div className="w-full min-h-screen bg-[#0f0f0f] text-white flex items-center justify-center px-4 py-6 -mt-25">
+      <div className="max-w-7xl w-full flex flex-col-reverse md:flex-row items-center justify-between gap-12">
+        <div className="flex-1 text-center md:text-left">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight mb-4">
+            <span className="text-red-400">I'm</span>{" "}
+            <span className="text-[#FFA500]">Sakshi</span>, a Frontend Developer{" "}
+            <span className="text-green-400">Enthusiast</span>
+          </h1>
+
+          <p className="text-gray-300 text-base sm:text-lg mt-2 max-w-xl">
+            I craft responsive, user-friendly web interfaces using React and the
+            MERN stack. Passionate about clean code, modern design, and turning
+            ideas into smooth user experiences.
+          </p>
+
+          <div className="mt-8 flex flex-col sm:flex-row gap-4">
+            <button
+              onClick={handleConnectClick}
+              className="bg-gradient-to-r from-purple-500 to-orange-400 text-white px-6 py-3 rounded-full font-semibold hover:opacity-90 transition"
+            >
+              Connect With Me!
+            </button>
+            <a
+              href={resumePDF}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="border border-gray-400 text-white px-6 py-3 rounded-full font-semibold hover:bg-white hover:text-black transition"
+            >
+              My Resume
+            </a>
+          </div>
+        </div>
+
+        <div className="flex-1 flex justify-center">
+          <div className="w-40 h-40 sm:w-78 sm:h-78 md:w-78 md:h-78 rounded-full overflow-hidden border-4 border-pink-300">
+            <img
+              src={profileImg}
+              alt="Sakshi"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
